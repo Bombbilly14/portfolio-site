3290c00cbcc9c89faba4d7dcb1e4c9782df690f9
William German <william.j.german2@gmail.com>
2023-03-02 17:10:40 -0800

    css and aboutme layout

--- portfolio_site_client/src/AboutMe.js
+++ portfolio_site_client/src/AboutMe.js
@@ -5,8 +5,14 @@ import './styles/aboutme.css'
 function AboutMe() {
   return (
     <div className='about-div'>
-      <h4>Former advertising and marketing professional to fullstack software development, with expertise in both creative and technical aspects. I bring a unique perspective to every project I work on, whether it's crafting sleek and intuitive user interfaces or scalable backend architectures. Driven by my persuit of knowledge, teamwork, and coffee 🏳️‍🌈</h4>
-      <img src="me.jpg" alt="William"/>
+      {/* <p>About me.</p> */}
+       {/* <br/> */}
+      <p>
+        Former advertising and marketing professional to fullstack software development, with expertise in both creative and technical aspects.
+         I bring a unique perspective to every project I work on, whether it's crafting sleek and intuitive user interfaces or scalable backend architectures.
+         Driven by my persuit of knowledge, teamwork, and coffee. 🏳️‍🌈
+        </p>
+      <img src="me2.jpg" alt="William" className="about-image"/>
     </div>
   )
 }
--- portfolio_site_client/src/Header.js
+++ portfolio_site_client/src/Header.js
@@ -4,7 +4,7 @@ import './styles/header.css'
 function Header() {
   return (
     <div className='header-div'>
-        <h2>Hey! My name is</h2>
+        <h2>Welcome! My name is</h2>
         <h1>William German</h1>
         <h2>A Fullstack software engineer</h2>
         <button>Contact Me!</button>
